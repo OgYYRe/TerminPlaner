775e8c7d26899ef3c232d4ebfbc09b8f2296c7a7
Oguzhan Cetinkaya <cetinkaya-oguzhan@hotmail.com>
2025-10-30 19:26:00 +0100

    initialize Spring Boot project with Maven setup and MySQL configuration

---
+++ src/main/java/ch/informatik/m223/TerminPlaner/TerminPlanerApplication.java
@@ -0,0 +1,13 @@
+package ch.informatik.m223.TerminPlaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminPlanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/informatik/m223/TerminPlaner/TerminPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.informatik.m223.TerminPlaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

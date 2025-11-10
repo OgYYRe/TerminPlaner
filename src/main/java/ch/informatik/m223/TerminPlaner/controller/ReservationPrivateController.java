104391d033cfcbcd3e9f2a8907c6ccadfb4af7b7
Oguzhan Cetinkaya <cetinkaya-oguzhan@hotmail.com>
2025-11-10 21:19:42 +0100

    implement private reservation retrieval and enhance display with details

--- src/main/java/ch/informatik/m223/TerminPlaner/controller/ReservationPrivateController.java
+++ src/main/java/ch/informatik/m223/TerminPlaner/controller/ReservationPrivateController.java
@@ -1,18 +1,31 @@
 package ch.informatik.m223.TerminPlaner.controller;
 
 
+import ch.informatik.m223.TerminPlaner.model.Reservation;
+import ch.informatik.m223.TerminPlaner.service.ReservationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Optional;
+
 @Controller
 public class ReservationPrivateController {
+    private final ReservationService reservationService;
+    public ReservationPrivateController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
     @GetMapping("/reservations/private/{code}")
     public String showPrivateReservations(@PathVariable String code, Model model) {
-        model.addAttribute("pageTitle", "Private Reservationen");
-        model.addAttribute("privateCode", code);
-        return "reservation_private";
+        Optional<Reservation> reservation = reservationService.findByCode(code);
+        if (reservation.isPresent()) {
+            model.addAttribute("pageTitle", "Private Reservation");
+            model.addAttribute("reservation", reservation.get());
+            return "reservation_private";
+        } else {
+            return "redirect:/";
+        }
     }
 }

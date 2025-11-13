08901b8509d7af6d0a6cb2dd1c1f7a1eab6c7121
luca-waldvogel <waldvogel.luca@hotmail.com>
2025-11-13 20:50:40 +0100

    refactor/data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
--- Räume einfügen (IDs 101 bis 105)
--- (Dieser Teil war wahrscheinlich in Ordnung, da der Fehler bei Statement #2 auftrat)
 INSERT IGNORE INTO rooms (id) VALUES
     (101),
     (102),
@@ -7,8 +5,6 @@ INSERT IGNORE INTO rooms (id) VALUES
     (104),
     (105);
 
--- Beispieldatensätze für Reservations einfügen
--- JETZT MIT DER SPALTE 'participants'
 INSERT INTO reservations (room_id, start_at, end_at, remark, public_code, private_code, participants) VALUES
 (
   101,
@@ -17,5 +13,5 @@ INSERT INTO reservations (room_id, start_at, end_at, remark, public_code, privat
   'Wöchentliches Team-Meeting',
   'p6qMh4uT',
   '8R38dgdO',
-  'Max Mustermann, Erika Mustermann' -- Beispiel-Teilnehmer
+  'Max Mustermann, Erika Mustermann'
 );
